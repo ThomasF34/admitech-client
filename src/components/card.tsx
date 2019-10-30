8a1cf096ecc58967fde966d8446365f1fba46505
Ines Missoum <inesmiss@live.fr>
2019-10-30 11:14:35 +0100

    [WIP] home design

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -17,10 +17,10 @@ class Card extends React.Component<IProps> {
         <div className="container">
           <div className="card" style={{ width: '18rem;' }}  >
             <img src={this.props.image} className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{this.props.title}</h5>
-              <p className="card-text">{this.props.text}</p>
-              <a href="connexion.tsx" className="btn btn-primary">{this.props.buttonTitle}</a>
+            <div className="card-body text-center">
+              <h2 className="card-title">{this.props.title}</h2>
+              <h5 className="card-text">{this.props.text}</h5>
+              <a href="connexion.tsx" className="btn btn-secondary">{this.props.buttonTitle}</a>
             </div>
           </div>
         </div>
--- src/components/containerCard.tsx
+++ src/components/containerCard.tsx
@@ -1,6 +1,8 @@
 import '../style/containerCard.css';
 import React from 'react';
-import student from '../img/fond-admitech.jpeg';
+import student from '../img/fond-student.jpeg';
+import polytech from '../img/fond-polytech.jpeg';
+import company from '../img/fond-company.jpeg';
 import Card from './card';
 
 
@@ -9,16 +11,16 @@ class ContainerCard extends React.Component {
     return (
 
 
-      <div className="root container fill ">
+      <div className="root container-fluid fill ">
         <div className="row rowCustom align-items-center">
           <div className="col-sm-12 col-md-4 ">
-            <Card title="titlee" buttonTitle="bouton" image={student} text="text" />
+            <Card title="Accès étudiants" buttonTitle="bouton" image={student} text="Voici un texte de présentation de la connexion étudiant" />
           </div>
           <div className="col-sm-12 col-md-4">
-            <Card title="title" buttonTitle="bouton" image={student} text="text" />
+            <Card title="Accès entreprises" buttonTitle="bouton" image={company} text="Voici un texte de présentation de la connexion entreprise" />
           </div>
           <div className="col-sm-12 col-md-4">
-            <Card title="title" buttonTitle="bouton" image={student} text="text" />
+            <Card title="Accès administration" buttonTitle="bouton" image={polytech} text="Voici un texte de présentation de la connexion admin" />
           </div>
         </div>
       </div>

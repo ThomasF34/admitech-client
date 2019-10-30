7be97efeb15f1591a3068f8253d9071aeca43b70
Ines Missoum <inesmiss@live.fr>
2019-10-30 16:50:13 +0100

    [done] click on card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 interface IProps {
   title: string,
-  buttonTitle: string,
+  clickLink: string,
   text: string,
   image: string
 }
@@ -15,14 +15,15 @@ class Card extends React.Component<IProps> {
 
       <div className="root">
         <div className="container">
-          <div className="card" style={{ width: '18rem;' }}  >
-            <img src={this.props.image} className="card-img-top" alt="..." />
-            <div className="card-body text-center">
-              <h2 className="card-title">{this.props.title}</h2>
-              <h5 className="card-text">{this.props.text}</h5>
-              <a href="connexion.tsx" className="btn btn-secondary">{this.props.buttonTitle}</a>
+          <a href={this.props.clickLink} style={{ textDecoration: 'none' }}>
+            <div className="card" style={{ width: '18rem;' }}  >
+              <img src={this.props.image} className="card-img-top" alt="..." />
+              <div className="card-body text-center">
+                <h2 className="card-title text-dark">{this.props.title}</h2>
+                <h5 className="card-text text-dark">{this.props.text}</h5>
+              </div>
             </div>
-          </div>
+          </a>
         </div>
       </div>
     );
--- src/components/containerCard.tsx
+++ src/components/containerCard.tsx
@@ -14,13 +14,13 @@ class ContainerCard extends React.Component {
       <div className="root container-fluid fill ">
         <div className="row rowCustom align-items-center">
           <div className="col-sm-12 col-md-4 ">
-            <Card title="Accès étudiants" buttonTitle="bouton" image={student} text="Voici un texte de présentation de la connexion étudiant" />
+            <Card title="Accès étudiants" clickLink="/connexion/etudiant" image={student} text="Voici un texte de présentation de la connexion étudiant" />
           </div>
           <div className="col-sm-12 col-md-4">
-            <Card title="Accès entreprises" buttonTitle="bouton" image={company} text="Voici un texte de présentation de la connexion entreprise" />
+            <Card title="Accès entreprises" clickLink="/connexion/entreprise" image={company} text="Voici un texte de présentation de la connexion entreprise" />
           </div>
           <div className="col-sm-12 col-md-4">
-            <Card title="Accès administration" buttonTitle="bouton" image={polytech} text="Voici un texte de présentation de la connexion admin" />
+            <Card title="Accès administration" clickLink="/connexion/administration" image={polytech} text="Voici un texte de présentation de la connexion admin" />
           </div>
         </div>
       </div>

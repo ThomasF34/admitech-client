7229aa16e8ac82060ef6c4c1ad2bc0bbc4b6baa3
Ines Missoum <inesmiss@live.fr>
2019-10-30 16:30:33 +0100

    [WIP] fix image and text on it

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Connexion from './components/connexion';
 import ContainerCard from './components/containerCard';
+import polytech from './img/fond-polytech.jpeg';
+import company from './img/fond-company.jpeg';
+import student from './img/fond-student.jpeg';
 
 
 export default function App() {
@@ -12,10 +15,13 @@ export default function App() {
             renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/connexion/etudiant">
-            <Connexion />
+            <Connexion image={student} text="ESPACE ETUDIANT"/>
           </Route>
           <Route path="/connexion/entreprise">
-            <Connexion />
+            <Connexion image={company} text="ESPACE ENTREPRISE"/>
+          </Route>
+          <Route path="/connexion/administration">
+            <Connexion image={polytech} text="ESPACE ADMINISTRATION"/>
           </Route>
           <Route path="/">
             <ContainerCard />
--- src/components/connexion.tsx
+++ src/components/connexion.tsx
@@ -1,36 +1,49 @@
 import React from 'react';
 import '../style/connexion.css';
 import ConnexionNav from './connexionNav';
-import polytech from '../img/fond-polytech.jpeg';
 import logo from '../img/polytechLogo.svg';
 
-const Connexion: React.FC = () => {
-  return (
+interface IProps {
+  text: string,
+  image: string
+}
 
-    <div className="root fill">
-      <div className="row fill no-gutters justify-content-end">
-        <div className="col-md-8">
-          <img src={polytech} className="img-fluid" alt="polytech" />
-        </div>
-        <div className="customCol col-sm-12 col-md-4">
-          <ConnexionNav />
-          <div className="container" style={{width:'40%', paddingTop:'10%'}}>
-            <img src={logo} className="img-fluid" alt="logo" />
-          </div>
-          <div className="container-title">
-            <div className="name-mainTitle">test</div>
-            <div className="decor">
-              <div className="decor-line">
+class Connexion extends React.Component<IProps> {
+  render() {
+    return (
+
+      <div className="root fill">
+        <div className="row fill no-gutters justify-content-end">
+          <div className="d-none d-md-block col-md-8" >
+            <div className="image-container">
+              <img src={this.props.image} className="img" alt="polytech" />
+              <div className="centered-text">
+                <h1 className="display-1"><u>{this.props.text}</u></h1>
+                <h1 className="display-4">PLATEFORME CANDIDATURE POLYTECH</h1>
               </div>
             </div>
           </div>
-          <span className='content'>test1</span>
+          <div className="customCol col-sm-12 col-md-4">
+            <ConnexionNav />
+            <div className="container" style={{ width: '40%', paddingTop: '10%' }}>
+              <img src={logo} className="img-fluid" alt="logo" />
+            </div>
+            <div className="container-title">
+              <div className="name-mainTitle">test</div>
+              <div className="decor">
+                <div className="decor-line">
+                </div>
+              </div>
+            </div>
+            <span className='content'>test1</span>
 
+          </div>
         </div>
       </div>
-    </div>
 
-  );
-};
+    );
+  }
+}
+
 
 export default Connexion;

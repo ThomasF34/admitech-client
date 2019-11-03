6a8a170074ece9ca24ebb0e4036a95fc7efdef84
Ines Missoum <inesmiss@live.fr>
2019-11-03 19:30:56 +0100

    [fix] test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MenuBar from './components/menubar';
+import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<MenuBar />, div);
+  ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });

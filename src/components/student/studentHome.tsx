77c9f75e35b809bd504a0ebaa5c9f47f650503ff
Ines Missoum <inesmiss@live.fr>
2019-11-03 22:48:55 +0100

    [wip] containers of elements on student home page

--- src/components/student/studentHome.tsx
+++ src/components/student/studentHome.tsx
@@ -1,5 +1,6 @@
-import '../../style/container.css';
 import React from 'react';
+import '../../style/home.css';
+import '../../style/container.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import company from '../../img/fond-student.jpeg';
 import user from '../../img/user.png';
@@ -15,18 +16,55 @@ class StudentHome extends React.Component {
 
           <div className="d-none d-md-block col-md-2 shadow-lg fill" >
             {/* should deal here with burger menu when small*/}
-            <StudentNav userImage={user} userName="Romain Planchet"/>
+            <StudentNav userImage={user} userName="Romain Planchet" />
 
           </div>
 
           <div className="col-sm-12 col-md-10 fill">
             <div className="image-container">
               <img src={company} className="img-background" alt="polytech" />
+              <div className="main-container align-items-end" >
+                <div className="row no-gutters" style={{ width: '100%', height: '90%'}}>
+
+                  <div className="col-sm-12 col-md-9 fill-container no-gutters" style={{paddingRight: '0%'}}>
+
+                    <div className="row no-gutters" style={{ width: '100%', height: '50%' }}>
+                      <div className="col-sm-12 col-md-6 fill-container" style={{ padding: '0.7%' }}>
+                        <div className="fill-container shadow-lg white">
+                        </div>
+                      </div>
+                      <div className="col-sm-12 col-md-6 fill-container " style={{ padding: '0.7%' }}>
+                        <div className="fill-container shadow-lg white">
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="row no-gutters" style={{ width: '100%', height: '50%' }}>
+                      <div className="col-sm-12 col-md-12 fill-container" style={{ padding: '0.7%' }}>
+                        <div className="fill-container shadow-lg white">
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
+
+                  {/*mail container*/}
+                  <div className="col-sm-12 col-md-3 fill-container" style={{ padding: '0.7%' }}>
+                    <div className="fill-container shadow-lg white">
+                    </div>
+                  </div>
+
+                </div>
+
+                <div className="row no-gutters" style={{ width: '100%', height: '10%', padding: '0.7%' }}>
+                  <div className="fill-container shadow-lg white">
+                  </div>
+                </div>
+
+              </div>
+
             </div>
-
-
           </div>
-
         </div>
       </div>
 

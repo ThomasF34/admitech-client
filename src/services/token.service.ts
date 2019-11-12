83145ebf693a394f5af53af2eb5eb302293fa4d1
MartinCayuelas <martincayuelas@gmail.com>
2019-11-12 20:09:54 +0100

    [WIP] Redirect when connected

--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -4,6 +4,6 @@ const getToken = () => { return localStorage.getItem(tokenName); };
 
 const setToken = (token: string) => { localStorage.setItem(tokenName, token); };
 
-const removeToken = () => { localStorage.removeItem(tokenName); };
+const removeToken = () => { localStorage.removeItem(tokenName);};
 
 export { getToken, setToken, removeToken };

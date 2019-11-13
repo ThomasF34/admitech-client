0b5de0edbd38e5e930bf0b865c4e8893d652f9bf
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2019-11-13 19:16:53 +0100

    Fix test & compile

--- src/components/home/card.tsx
+++ src/components/home/card.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Link } from 'react-router-dom';
 
 interface IProps {
   title: string,
@@ -17,14 +16,14 @@ class Card extends React.Component<IProps> {
       <div className="root">
         <div className="container">
           <a href={this.props.clickLink} style={{ textDecoration: 'none' }}>
-            <div className="card" style={{ width: '18rem' }}  >
+            <div className="card" style={{ width: '18rem;' }}  >
               <img src={this.props.image} className="card-img-top" alt="..." />
               <div className="card-body text-center">
                 <h2 className="card-title text-dark">{this.props.title}</h2>
                 <h5 className="card-text text-dark">{this.props.text}</h5>
               </div>
             </div>
-          </Link>
+          </a>
         </div>
       </div>
     );

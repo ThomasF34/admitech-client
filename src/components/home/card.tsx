dce474fe4ccf9c8808e68e53f1ea9c8f4a64f455
Ines Missoum <inesmiss@live.fr>
2019-11-13 19:00:13 +0100

    [fix] test

--- src/components/home/card.tsx
+++ src/components/home/card.tsx
@@ -16,7 +16,7 @@ class Card extends React.Component<IProps> {
       <div className="root">
         <div className="container">
           <a href={this.props.clickLink} style={{ textDecoration: 'none' }}>
-            <div className="card" style={{ width: '18rem;' }}  >
+            <div className="card" style={{ width: '18rem' }}  >
               <img src={this.props.image} className="card-img-top" alt="..." />
               <div className="card-body text-center">
                 <h2 className="card-title text-dark">{this.props.title}</h2>

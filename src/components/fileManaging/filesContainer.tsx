81980623fe719f6c12675ee7427cc9b73a048abd
MartinCayuelas <martincayuelas@gmail.com>
2019-11-16 18:12:34 +0100

    [WIP] delete file in DB and Upload to S3

--- src/components/fileManaging/filesContainer.tsx
+++ src/components/fileManaging/filesContainer.tsx
@@ -4,195 +4,203 @@ import '../../style/fileManaging.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import { handleUpload } from '../../services/filesManaging.service';
-import { getListOfTypesOfFiles, getTypeConverted } from './filesManaging.helper';
+import { deleteAttachmentInApplication } from '../../services/application.service';
+import { getListOfTypesOfFiles, getTypeConverted } from '../../helpers/filesManaging.helper';
+
 
 //State and Props
 interface IProps {
-    attachments: Array<IAttachement>,
-    candId: number
+  attachments: Array<IAttachement>,
+  candId: number
 }
 
 interface IState {
-    typesList: Array<IOption>,
-    currentFile: any,
-    curentTypeFile: string
-    filesAdded: Array<IAttachement>,
-    error: boolean,
-    added: boolean,
-    deleted: boolean,
-    formatNotSupproted: boolean,
-    disabled: boolean,
-    sizeNotSupproted: boolean
+  typesList: Array<IOption>,
+  currentFile: any,
+  curentTypeFile: string
+  filesAdded: Array<IAttachement>,
+  error: boolean,
+  added: boolean,
+  deleted: boolean,
+  formatNotSupproted: boolean,
+  disabled: boolean,
+  sizeNotSupproted: boolean
 }
 //Utils Interfaces
 
 interface IAttachement {
-    id?: number,
-    attach_type: string,
-    url: string,
-    file: any
+  id?: number,
+  attach_type: string,
+  url: string,
+  file: any
 }
 
 interface IOption {
-    attach_type: string,
-    typeConverted: string
+  attach_type: string,
+  typeConverted: string
 }
 
 class FileContainer extends React.Component<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
-        this.state = {
-            typesList: getListOfTypesOfFiles(this.props.attachments),
-            currentFile: null,
-            curentTypeFile: '',
-            filesAdded: this.props.attachments,
-            error: false,
-            added: false,
-            deleted: false,
-            formatNotSupproted: false,
-            disabled: true,
-            sizeNotSupproted: false
-        };
-        this.removeElementInTypes = this.removeElementInTypes.bind(this);
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      typesList: getListOfTypesOfFiles(this.props.attachments),
+      currentFile: null,
+      curentTypeFile: '',
+      filesAdded: this.props.attachments,
+      error: false,
+      added: false,
+      deleted: false,
+      formatNotSupproted: false,
+      disabled: true,
+      sizeNotSupproted: false
+    };
+    this.removeElementInTypes = this.removeElementInTypes.bind(this);
+  }
+
+  handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
+    let reader = new FileReader();
+    let file = e.target.files![0];
+    const extension = file.name.split('.')
+
+    this.setState({
+      currentFile: file,
+      added: false,
+      deleted: false
+    });
+
+    if (file.size > 1000000) {
+      this.setState({
+        sizeNotSupproted: true,
+        disabled: true
+      });
+    } else { this.setState({ sizeNotSupproted: false, }); }
+    if (extension[extension.length - 1] === 'pdf' || extension[extension.length - 1] === 'png' || extension[extension.length - 1] === 'jpeg') {
+      this.setState({
+        formatNotSupproted: false,
+        disabled: false,
+        sizeNotSupproted: false,
+      });
+      reader.readAsDataURL(file);
+    } else {
+      this.setState({
+        formatNotSupproted: true,
+        disabled: true
+      });
     }
+  }
 
-    handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
-        let reader = new FileReader();
-        let file = e.target.files![0];
-        const extension = file.name.split('.')
+  handleTypeChange(attach_type: string) {
+    this.setState({
+      curentTypeFile: attach_type,
+      added: false,
+      deleted: false
+    });
+  }
 
-        this.setState({
-            currentFile: file,
-            added: false,
-            deleted: false
-        });
+  async addNewFile(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
+    if (this.state.curentTypeFile === '' || this.state.currentFile === null) {
+      this.setState({ error: true });
+    } else {
+      let urlFile: string = '';
 
-        if (file.size > 1000000) {
-            this.setState({
-                sizeNotSupproted: true,
-                disabled: true
-            });
-        } else { this.setState({ sizeNotSupproted: false, }); }
-        if (extension[extension.length - 1] === 'pdf' || extension[extension.length - 1] === 'png' || extension[extension.length - 1] === 'jpeg') {
-            this.setState({
-                formatNotSupproted: false,
-                disabled: false,
-                sizeNotSupproted: false,
-            });
-            reader.readAsDataURL(file);
-        } else {
-            this.setState({
-                formatNotSupproted: true,
-                disabled: true
-            });
-        }
+      if (this.props.candId !== null) {
+        /*  handleUpload(this.state.currentFile).then(url =>{
+                          if(url){
+                              urlFile = url
+                          }
+                      });*/
+      }
+
+      const newAttachement: IAttachement = { url: urlFile, attach_type: this.state.curentTypeFile, file: this.state.currentFile }
+      this.removeElementInTypes();
+      this.setState(previousState => ({
+        filesAdded: [...previousState.filesAdded, newAttachement],
+        curentTypeFile: '',
+        currentFile: null,
+        error: false,
+        added: true
+      }));
     }
+  }
 
-    handleTypeChange(attach_type: string) {
-        this.setState({
-            curentTypeFile: attach_type,
-            added: false,
-            deleted: false
-        });
+  removeElementInTypes() {
+    const listTypes = this.state.typesList
+    const currentFile = this.state.curentTypeFile;
+    const newList = listTypes.filter(function (type) {
+      return type.attach_type !== currentFile;
+    });
+    this.setState({
+      typesList: newList
+    })
+  }
+
+  resetListOfTypes(typeFile: string) {
+    const type: IOption = { attach_type: typeFile, typeConverted: getTypeConverted(typeFile) }
+    this.setState(previousState => ({
+      typesList: [...previousState.typesList, type]
+    }));
+  }
+
+  removeElemFromListAdded(event: React.MouseEvent<HTMLButtonElement, MouseEvent>, file: IAttachement) {
+    event.preventDefault();
+    if (this.props.candId !== null) {
+      console.log('candid ok')
+      // deleteAttachmentInApplication(this.props.candId, file.id!);
     }
+    const listAdded = this.state.filesAdded;
+    const newList = listAdded.filter(function (elem) {
+      return elem.attach_type !== file.attach_type;
+    });
+    this.setState({
+      filesAdded: newList,
+      deleted: true,
+      added: false
+    });
+    this.resetListOfTypes(file.attach_type);
+  }
 
-    async addNewFile(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-        e.preventDefault();
-        if (this.state.curentTypeFile === '' || this.state.currentFile === null) {
-            this.setState({ error: true });
-        } else {
-            let urlFile: string = '';
-
-
-            /*  handleUpload(this.state.currentFile).then(url =>{
-                  if(url){
-                      urlFile = url
-                  }
-              });*/
-            const newAttachement: IAttachement = { url: urlFile, attach_type: this.state.curentTypeFile, file: this.state.currentFile }
-            this.removeElementInTypes();
-            this.setState(previousState => ({
-                filesAdded: [...previousState.filesAdded, newAttachement],
-                curentTypeFile: '',
-                currentFile: null,
-                error: false,
-                added: true
-            }));
-        }
-    }
-
-    removeElementInTypes() {
-        const listTypes = this.state.typesList
-        const currentFile = this.state.curentTypeFile;
-        const newList = listTypes.filter(function (type) {
-            return type.attach_type !== currentFile;
-        });
-        this.setState({
-            typesList: newList
-        })
-    }
-
-    resetListOfTypes(typeFile: string) {
-        const type: IOption = { attach_type: typeFile, typeConverted: getTypeConverted(typeFile) }
-        this.setState(previousState => ({
-            typesList: [...previousState.typesList, type]
-        }));
-    }
-
-    removeElemFromListAdded(event: React.MouseEvent<HTMLButtonElement, MouseEvent>, file: IAttachement) {
-        event.preventDefault();
-        const listAdded = this.state.filesAdded;
-        const newList = listAdded.filter(function (elem) {
-            return elem.attach_type !== file.attach_type;
-        });
-        this.setState({
-            filesAdded: newList,
-            deleted: true,
-            added: false
-        });
-        this.resetListOfTypes(file.attach_type);
-    }
-
-    render() {
-        return (
-            <div className='container bg-light mt-5 p-3'>
-                <form className="row">
-                    <div className="form-group ml-2 mb-2">
-                        <select id="file-select" className='form-control'>
-                            {this.state.typesList.map(file => (
-                                <option key={file.attach_type} value={file.attach_type} onClick={() => this.handleTypeChange(file.attach_type)} >{file.typeConverted}</option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className="form-group mx-sm-3 mb-2">
-                        <label htmlFor="file" className="label-file btn btn-info">Télécharger</label>
-                        <input id="file" type="file" className="form-control-file input-file" onChange={(e) => this.handleImageChange(e)} />
-                    </div>
-                    <div className="form-group mb-2">
-                        {this.state.currentFile !== null ? (
-                            <button className='btn btn-success' disabled={this.state.disabled} onClick={(e) => this.addNewFile(e)}>Ajouter</button>) : null}
-                    </div>
-                </form>
-                <div>
-                    {this.state.currentFile !== null ? <small className='display'>Fichier téléchargé : {this.state.currentFile.name} -- {this.state.currentFile.size / 1000000} MB</small> : null}
-                    {this.state.error ? <div className="alert alert-danger mt-2 text-center" role="alert"> Choisissez un fichier et un type de fichier S.V.P</div> : null}
-                    {this.state.added ? <div className="alert alert-success mt-2 text-center" role="alert"> Ajout réussi !</div> : null}
-                    {this.state.deleted ? <div className="alert alert-success mt-2 text-center" role="alert"> Suppression réussie !</div> : null}
-                    {this.state.formatNotSupproted ? <div className="alert alert-danger mt-2 text-center" role="alert"> Format de fichier non supporté (Seulement: png, jpeg ou pdf)</div> : null}
-                    {this.state.sizeNotSupproted ? <div className="alert alert-danger mt-2 text-center" role="alert">Taille du fichier maximale : 1MB</div> : null}
-                </div>
-                <div>
-                    <hr />
-                    <h4>Mes fichiers téléchargés</h4>
-                    {this.state.filesAdded.map(file => (
-                        <div className='mb-2' key={file.id}><span className="badge badge-success">OK</span> <span className='text-info'>{getTypeConverted(file.attach_type)}</span> :
+  render() {
+    return (
+      <div className='container bg-light mt-5 p-3'>
+        <form className="row">
+          <div className="form-group ml-2 mb-2">
+            <select id="file-select" className='form-control'>
+              {this.state.typesList.map(file => (
+                <option key={file.attach_type} value={file.attach_type} onClick={() => this.handleTypeChange(file.attach_type)} >{file.typeConverted}</option>
+              ))}
+            </select>
+          </div>
+          <div className="form-group mx-sm-3 mb-2">
+            <label htmlFor="file" className="label-file btn btn-info">Télécharger</label>
+            <input id="file" type="file" className="form-control-file input-file" onChange={(e) => this.handleImageChange(e)} />
+          </div>
+          <div className="form-group mb-2">
+            {this.state.currentFile !== null ? (
+              <button className='btn btn-success' disabled={this.state.disabled} onClick={(e) => this.addNewFile(e)}>Ajouter</button>) : null}
+          </div>
+        </form>
+        <div>
+          {this.state.currentFile !== null ? <small className='display'>Fichier téléchargé : {this.state.currentFile.name} -- {this.state.currentFile.size / 1000000} MB</small> : null}
+          {this.state.error ? <div className="alert alert-danger mt-2 text-center" role="alert"> Choisissez un fichier et un type de fichier S.V.P</div> : null}
+          {this.state.added ? <div className="alert alert-success mt-2 text-center" role="alert"> Ajout réussi !</div> : null}
+          {this.state.deleted ? <div className="alert alert-success mt-2 text-center" role="alert"> Suppression réussie !</div> : null}
+          {this.state.formatNotSupproted ? <div className="alert alert-danger mt-2 text-center" role="alert"> Format de fichier non supporté (Seulement: png, jpeg ou pdf)</div> : null}
+          {this.state.sizeNotSupproted ? <div className="alert alert-danger mt-2 text-center" role="alert">Taille du fichier maximale : 1MB</div> : null}
+        </div>
+        <div>
+          <hr />
+          <h4>Mes fichiers téléchargés</h4>
+          {this.state.filesAdded.map(file => (
+            <div className='mb-2' key={file.id}><span className="badge badge-success">OK</span> <span className='text-info'>{getTypeConverted(file.attach_type)}</span> :
                    {file.file === null ? <span className='text-secondary'> <a href={file.url} target='_blank' rel="noopener noreferrer">voir</a></span> : <span className='text-secondary'> {file.file.name} </span>}
-                            <button className='btn btn-sm btn-danger ml-1' onClick={(e) => this.removeElemFromListAdded(e, file)} > <FontAwesomeIcon icon={faTimesCircle} /></button> </div>
-                    ))}
-                </div>
-            </div>
-        );
-    }
+              <button className='btn btn-sm btn-danger ml-1' onClick={(e) => this.removeElemFromListAdded(e, file)} > <FontAwesomeIcon icon={faTimesCircle} /></button> </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FileContainer;
--- src/helpers/filesManaging.helper.ts
+++ src/helpers/filesManaging.helper.ts
@@ -1,51 +1,51 @@
 
 interface IAttachement {
-    id?: number,
-    attach_type: string,
-    url: string,
-    file: any
+  id?: number,
+  attach_type: string,
+  url: string,
+  file: any
 }
 
 interface IOption {
-    attach_type: string,
-    typeConverted: string
+  attach_type: string,
+  typeConverted: string
 }
 
 const getListOfTypesOfFiles = (attachments: IAttachement[]): IOption[] => {
 
-    let attachmentsIOptions: IOption[] = attachments.map((attach) => {
-        const option: IOption = { attach_type: attach.attach_type, typeConverted: getTypeConverted(attach.attach_type) }
-        return option;
-    });
-    const default_value: IOption = { attach_type: '', typeConverted: '--Type de fichier--' };
-    const cover_letter: IOption = { attach_type: 'cover_letter', typeConverted: 'Lettre de motivation' };
-    const cv: IOption = { attach_type: 'cv', typeConverted: 'CV' };
-    const bac_marks: IOption = { attach_type: 'bac_marks', typeConverted: 'Notes du Bac' };
-    const year_marks: IOption = { attach_type: 'year_marks', typeConverted: 'Notes' };
-    const current_year_marks: IOption = { attach_type: 'current_year_marks', typeConverted: 'Notes année courante' };
-    const notice_further_study: IOption = { attach_type: 'notice_further_study', typeConverted: 'Avis poursuite d\'études' };
+  let attachmentsIOptions: IOption[] = attachments.map((attach) => {
+    const option: IOption = { attach_type: attach.attach_type, typeConverted: getTypeConverted(attach.attach_type) }
+    return option;
+  });
+  const default_value: IOption = { attach_type: '', typeConverted: '--Type de fichier--' };
+  const cover_letter: IOption = { attach_type: 'cover_letter', typeConverted: 'Lettre de motivation' };
+  const cv: IOption = { attach_type: 'cv', typeConverted: 'CV' };
+  const bac_marks: IOption = { attach_type: 'bac_marks', typeConverted: 'Notes du Bac' };
+  const year_marks: IOption = { attach_type: 'year_marks', typeConverted: 'Notes' };
+  const current_year_marks: IOption = { attach_type: 'current_year_marks', typeConverted: 'Notes année courante' };
+  const notice_further_study: IOption = { attach_type: 'notice_further_study', typeConverted: 'Avis poursuite d\'études' };
 
-    let attachmentsBacis = [default_value, cover_letter, cv, bac_marks, year_marks, current_year_marks, notice_further_study];
+  let attachmentsBacis = [default_value, cover_letter, cv, bac_marks, year_marks, current_year_marks, notice_further_study];
 
-    let indexesToPop: number[] = [];
-    attachmentsIOptions.forEach((a) => {
-        attachmentsBacis.filter((elem => {
-            if (elem.attach_type === a.attach_type) { indexesToPop.push(attachmentsBacis.indexOf(elem)) }
-            return true;
-        }));
-    })
+  let indexesToPop: number[] = [];
+  attachmentsIOptions.forEach((a) => {
+    attachmentsBacis.filter((elem => {
+      if (elem.attach_type === a.attach_type) { indexesToPop.push(attachmentsBacis.indexOf(elem)) }
+      return true;
+    }));
+  })
 
-    function arrayRemove(arr: IOption[], value: IOption) { return arr.filter((ele) => { return ele !== value; }); }
+  function arrayRemove(arr: IOption[], value: IOption) { return arr.filter((ele) => { return ele !== value; }); }
 
-    for (let i in indexesToPop) { attachmentsBacis = arrayRemove(attachmentsBacis, attachmentsBacis[indexesToPop[i]]); }
-    return attachmentsBacis;
+  for (let i in indexesToPop) { attachmentsBacis = arrayRemove(attachmentsBacis, attachmentsBacis[indexesToPop[i]]); }
+  return attachmentsBacis;
 }
 
 const getTypeConverted = (attach_type: string): string => {
-    const typesConverted = ['--Type de fichier--', 'Lettre de motivation', 'CV', 'Notes du Bac', 'Notes', 'Notes année courante', 'Avis poursuite d\'études'];
-    const attachTypes = ['', 'cover_letter', 'cv', 'bac_marks', 'year_marks', 'current_year_marks', 'notice_further_study'];
-    const index = attachTypes.indexOf(attach_type);
-    return typesConverted[index];
+  const typesConverted = ['--Type de fichier--', 'Lettre de motivation', 'CV', 'Notes du Bac', 'Notes', 'Notes année courante', 'Avis poursuite d\'études'];
+  const attachTypes = ['', 'cover_letter', 'cv', 'bac_marks', 'year_marks', 'current_year_marks', 'notice_further_study'];
+  const index = attachTypes.indexOf(attach_type);
+  return typesConverted[index];
 }
 
 

30a39aa28859d422c204fe14bd99fc79527e7776
Ines Missoum <inesmiss@live.fr>
2019-11-18 13:42:08 +0100

    [wip] check on sign up form

--- src/components/connexion/sighUpForm.tsx
+++ src/components/connexion/sighUpForm.tsx
@@ -2,27 +2,45 @@ import React from 'react';
 import UserSignUpDto from '../../models/user/userSignUpDto';
 import { signUp } from '../../services/auth.service';
 import { Redirect } from 'react-router';
+import InfoPopUp from '../helpers/InfoPopUp';
 
 interface ISignUpForm {
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
   submit: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
 
+interface IErrors {
+  email?: string,
+  first_name?: string,
+  last_name?: string,
+  password?: string,
+  confirm_password?: string,
+  check?: string
+}
 interface IState {
   email: string,
-  firstName: string,
-  lastName: string,
+  first_name: string,
+  last_name: string,
   password: string,
-  confirmPassword: string,
-  error: string,
-  MatchingPwdError: string,
-  signUpDone: boolean
+  confirm_password: string,
+  check: boolean,
+  errors: IErrors,
+  signUpDone: boolean,
+  nonPlanError: boolean,
+  failure: boolean,
+  success: boolean
 }
 
 interface IProps {
   redirectPath: string,
-  role:string
+  role: string
 }
+
+const requiredField: string = 'Ce champ est obligatoire.'
+const accept: string = 'Veuillez accepter les conditions.'
+const matchingPwdError: string = 'Les mots de passes ne sont pas identiques.'
+const passwordSize: string = 'Votre mot de passe doit comporter au moins 6 caractères'
+const validEmail: string = 'Adresse non valide.'
 
 class SignUpForm extends React.Component<IProps, IState> implements ISignUpForm {
 
@@ -30,64 +48,173 @@ class SignUpForm extends React.Component<IProps, IState> implements ISignUpForm 
     super(props);
     this.state = {
       email: '',
-      firstName: '',
-      lastName: '',
+      first_name: '',
+      last_name: '',
       password: '',
-      confirmPassword: '',
-      error: '',
-      MatchingPwdError: '',
-      signUpDone: false
+      confirm_password: '',
+      check: false,
+      errors: {
+        email: requiredField,
+        first_name: requiredField,
+        last_name: requiredField,
+        password: requiredField,
+        confirm_password: requiredField,
+        check: accept
+      },
+      signUpDone: false,
+      nonPlanError: false,
+      failure: false,
+      success: false
     };
+  }
+
+  closeErrorPopUP = () => {
+    if (this.state.nonPlanError === true)
+      this.setState({
+        nonPlanError: false
+      });
+  }
+  closeSuccessPopUP = () => {
+    if (this.state.success === true)
+      this.setState({
+        signUpDone: true,
+        success: false
+      });
+  }
+  closeFailurePopUP = () => {
+    if (this.state.failure === true)
+      this.setState({
+        failure: false
+      });
+  }
+  error = (e: any) => {
+    this.setState({
+      nonPlanError: true
+    })
+    console.log(e)
+  }
+
+  validForm = (): boolean => {
+    let error = this.state.errors
+    return (
+      (error.email === undefined || error.email === '') &&
+      (error.first_name === undefined || error.first_name === '') &&
+      (error.last_name === undefined || error.last_name === '') &&
+      (error.password === undefined || error.password === '') &&
+      (error.confirm_password === undefined || error.confirm_password === '') &&
+      (error.check === undefined || error.check === '')
+    )
   }
 
   submit = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
 
     event.preventDefault();
     const validConnexion = () => this.setState({
-      signUpDone: true
+      success: true
     });
 
-    if (this.state.password !== this.state.confirmPassword)
-      this.setState({
-        MatchingPwdError: 'Les mots de passes ne sont pas identiques.'
-      });
-    else {
-      const user = new UserSignUpDto(this.state.email, this.state.firstName, this.state.lastName, this.state.password, this.props.role);
+    if (this.validForm()) {
+      const user = new UserSignUpDto(this.state.email, this.state.first_name, this.state.last_name, this.state.password, this.props.role);
       //call to the api
       signUp(user)
         .then(validConnexion)
-        .catch();
+        .catch((e) => {
+          if (e.status === 400)
+            missingFields(e)
+          else
+            this.error(e)
+        });
+    } else {
+      this.setState({
+        failure: true
+      })
     }
+
+    const missingFields = (reponse: any) => {
+      this.setState({
+        errors: reponse.data,
+        failure: true
+      })
+    }
+
+  }
+
+  changeEmail = (value: string) => {
+    let newErrors = this.state.errors
+
+    newErrors.email = value === '' ? requiredField : (value.includes('@') ? "" : validEmail)
+    this.setState({
+      email: value,
+      errors: newErrors
+    });
+  }
+
+  changeFistname = (value: string) => {
+    let newErrors = this.state.errors
+    newErrors.first_name = value === '' ? requiredField : ""
+    this.setState({
+      first_name: value,
+      errors: newErrors
+    });
+  }
+
+  changeLastname = (value: string) => {
+    let newErrors = this.state.errors
+    newErrors.last_name = value === '' ? requiredField : ""
+    this.setState({
+      last_name: value,
+      errors: newErrors
+    });
+  }
+
+  acceptCheck = () => {
+    let newErrors = this.state.errors
+    newErrors.check = this.state.check === true ? accept : ""
+    this.setState({
+      check: !this.state.check,
+      errors: newErrors
+    });
+  }
+
+  confirmPassword = (value: string) => {
+    let newErrors = this.state.errors
+    newErrors.confirm_password = value === '' ? requiredField : (this.state.password !== value ? matchingPwdError : "")
+    this.setState({
+      confirm_password: value,
+      errors: newErrors
+    });
+  }
+
+  changePassword = (value: string) => {
+    let newErrors = this.state.errors
+    newErrors.password = value === '' ? requiredField : (this.state.password.length < 5 ? passwordSize : "")
+    this.setState({
+      password: value,
+      errors: newErrors
+    });
   }
 
   handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     if (event.target != null) {
       switch (event.target.name) {
-      case 'email':
-        this.setState({
-          email: event.target.value
-        });
-        break;
-      case 'firstname':
-        this.setState({
-          firstName: event.target.value,
-        });
-        break;
-      case 'lastname':
-        this.setState({
-          lastName: event.target.value
-        });
-        break;
-      case 'password':
-        this.setState({
-          password: event.target.value
-        });
-        break;
-      case 'confirm_password':
-        this.setState({
-          confirmPassword: event.target.value
-        });
-        break;
+        case 'email':
+          this.changeEmail(event.target.value)
+          break;
+        case 'first_name':
+          this.changeFistname(event.target.value)
+          break;
+        case 'last_name':
+          this.changeLastname(event.target.value)
+          break;
+        case 'password':
+          this.changePassword(event.target.value)
+          break;
+        case 'confirm_password':
+          this.confirmPassword(event.target.value)
+          break;
+        case 'check':
+          this.acceptCheck()
+          break;
       }
     }
   }
@@ -98,39 +225,44 @@ class SignUpForm extends React.Component<IProps, IState> implements ISignUpForm 
     else {
       return (
 
-        <div className="container" style={{ width: '75%', marginTop: '15%' }}>
+        <div className="container" style={{ width: '75%', marginTop: '9%' }}>
           <form className="needs-validation">
 
             <div className="form-group" style={{ marginBottom: '8%' }} >
               <div className="form-group">
                 <input name="email" type="text" className="form-control form-control-lg text-center" placeholder="Adresse email"
                   onChange={this.handleChange} value={this.state.email} required />
+                <h6 className='text-danger'>{this.state.errors.email}</h6>
               </div>
               <div className="form-group">
-                <input name="lastname" type="text" className="form-control form-control-lg text-center" placeholder="Nom"
-                  onChange={this.handleChange} value={this.state.lastName} required />
+                <input name="last_name" type="text" className="form-control form-control-lg text-center" placeholder="Nom"
+                  onChange={this.handleChange} value={this.state.last_name} />
+                <h6 className='text-danger'>{this.state.errors.last_name}</h6>
               </div>
               <div className="form-group" >
-                <input name="firstname" type="text" className="form-control form-control-lg text-center" placeholder="Prénom"
-                  onChange={this.handleChange} value={this.state.firstName} required />
+                <input name="first_name" type="text" className="form-control form-control-lg text-center" placeholder="Prénom"
+                  onChange={this.handleChange} value={this.state.first_name} />
+                <h6 className='text-danger'>{this.state.errors.first_name}</h6>
               </div>
             </div>
 
             <div className="form-group" style={{ marginBottom: '8%' }} >
               <div className="form-group">
-                <input name="password" type="password" className="form-control form-control-lg text-center" placeholder="Mot de passe" minLength={6}
-                  onChange={this.handleChange} value={this.state.password} required />
+                <input name="password" type="password" className="form-control form-control-lg text-center" placeholder="Mot de passe"
+                  onChange={this.handleChange} value={this.state.password} />
+                <h6 className='text-danger'>{this.state.errors.password}</h6>
               </div>
               <div className="form-group">
-                <input name="confirm_password" type="password" className="form-control form-control-lg text-center" placeholder="Répétez votre mot de passe" minLength={6}
-                  onChange={this.handleChange} value={this.state.confirmPassword} required />
+                <input name="confirm_password" type="password" className="form-control form-control-lg text-center" placeholder="Répétez votre mot de passe"
+                  onChange={this.handleChange} value={this.state.confirm_password} />
               </div>
-              <h6 className="text-danger">{this.state.MatchingPwdError}</h6>
+              <h6 className="text-danger">{this.state.errors.confirm_password}</h6>
             </div>
 
             <div className="form-group">
               <div className="form-check">
-                <input id="check" className="form-check-input input-lg" type="checkbox" required />
+                <h6 className="text-danger">{this.state.errors.check}</h6>
+                <input name="check" className="form-check-input input-lg" type="checkbox" onChange={this.handleChange} />
                 J&apos;accepte les conditions générales d&apos;utilisation
               </div>
             </div>
@@ -141,6 +273,12 @@ class SignUpForm extends React.Component<IProps, IState> implements ISignUpForm 
             </div>
             <h5>Votre identifiant sera unique aux plateformes de Polytech.</h5>
           </form>
+          <InfoPopUp isError={true} title="ERREUR" content="Une erreur innatendue s'est produite."
+            show={this.state.nonPlanError} onClose={this.closeErrorPopUP} />
+          <InfoPopUp isError={true} title="ERREUR" content="Veuillez compléter tous les champs."
+            show={this.state.failure} onClose={this.closeFailurePopUP} />
+          <InfoPopUp isError={false} title="INSCRIPTION" content="Votre inscription est validée. Veuillez vous connecter."
+            show={this.state.success} onClose={this.closeSuccessPopUP} />
         </div>
       );
     }

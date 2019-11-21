825d8d621600c882f4efe1a380b8267f77d18df3
Ines Missoum <inesmiss@live.fr>
2019-11-21 11:00:40 +0100

    [fix] lint

--- src/helpers/statusHelper.tsx
+++ src/helpers/statusHelper.tsx
@@ -1,93 +1,93 @@
 export const draftStep = (status: string | undefined): boolean => {
   {/* if new application */ }
   if (status === undefined)
-    return true
+    return true;
   else {
-    let s = parseInt(status)
+    let s = parseInt(status);
     if (s === 0 || s === 1)
-      return true
+      return true;
     else
-      return false
+      return false;
   }
-}
+};
 
 export const notCompleteApplication = (status: string | undefined): boolean => {
   {/* if new application */ }
   if (status === undefined)
-    return true
+    return true;
   else {
-    let s = parseInt(status)
+    let s = parseInt(status);
     if (s < 4)
-      return true
+      return true;
     else
-      return false
+      return false;
   }
-}
+};
 
 export const giveJury = (status: string | undefined): boolean => {
   {/* if new application */ }
   if (status === undefined)
-    return false
+    return false;
   else {
-    let s = parseInt(status)
+    let s = parseInt(status);
     if (s === 6)
-      return true
+      return true;
     else
-      return false
+      return false;
   }
-}
+};
 
 export const chooseInterview = (status: string | undefined): boolean => {
   {/* if new application */ }
   if (status === undefined)
-    return false
+    return false;
   else {
-    let s = parseInt(status)
+    let s = parseInt(status);
     if (s === 7)
-      return true
+      return true;
     else
-      return false
+      return false;
   }
-}
+};
 
 export const giveMCQ = (status: string | undefined): boolean => {
   {/* if new application */ }
   if (status === undefined)
-    return false
+    return false;
   else {
-    let s = parseInt(status)
+    let s = parseInt(status);
     if (s === 4)
-      return true
+      return true;
     else
-      return false
+      return false;
   }
-}
+};
 
 export const doMCQ = (status: string | undefined): boolean => {
   {/* if new application */ }
   if (status === undefined)
-    return false
+    return false;
   else {
-    let s = parseInt(status)
+    let s = parseInt(status);
     if (s === 5)
-      return true
+      return true;
     else
-      return false
+      return false;
   }
-}
+};
 
 export const decision = (status: string | undefined): boolean => {
   {/* if new application */ }
   if (status === undefined)
-    return false
+    return false;
   else {
-    let s = parseInt(status)
+    let s = parseInt(status);
     if (s < 10)
-      return true
+      return true;
     else
-      return false
+      return false;
   }
-}
+};
 
 
 

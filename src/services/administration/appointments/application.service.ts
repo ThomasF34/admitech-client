3212c6d2f37e610f48f1adcf334ae985c269da52
Fatima <fatima.machhouri1@gmail.com>
2019-11-21 16:24:13 +0100

    [FIX] refacto

--- src/services/administration/appointments/application.service.ts
+++ src/services/administration/appointments/application.service.ts
@@ -3,10 +3,6 @@ import * as config from './../../configApi.service';
 import { getToken } from './../../token.service';
 
 const addSlots = async (formation: string, startDate: string, endDate: string, duration: string) => {
-  console.log(formation);
-  console.log(startDate);
-  console.log(endDate);
-  console.log(duration);
   const res = await axios
     .post(`${config.API_URL}/entretien`, {"formation": formation, "begining_hour": startDate, "ending_hour": endDate, "duration": +duration},
       {

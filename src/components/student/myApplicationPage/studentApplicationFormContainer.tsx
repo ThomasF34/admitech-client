6e2c9d3d8af833dd4a693dd34bf8d0f90e768c89
Ines Missoum <inesmiss@live.fr>
2019-11-22 21:19:44 +0100

    [wip] size button

--- src/components/student/myApplicationPage/studentApplicationFormContainer.tsx
+++ src/components/student/myApplicationPage/studentApplicationFormContainer.tsx
@@ -39,23 +39,21 @@ class StudentApplicationFormContainer extends React.Component<IProps, IState>{
     return (
       <div className="row">
 
-        <div>
-          {doMCQ(this.state.values.status) ? (
-            <div className="col-5 col-sm-5 col-lg-2">
-              <button className="btn btn-secondary btn-lg btn-block shadow" type="submit" >QCM</button>
-              <small className="text-secondary">Effectuer votre QCM</small>
-            </div>
-          ) : null}
-          {chooseInterview(this.state.values.status) ? (
-            <div className="col-5 col-sm-5 col-lg-2">
-              <button className="btn btn-secondary btn-lg btn-block shadow" type="submit" >Entretien</button>
-              <small className="text-secondary">Programmer votre entretien</small>
-            </div>
-          ) : null}
-        </div>
-
-
         <div className="col-sm-12 col-md-12 fill">
+          <div className="col-sm-6 col-md-3 fill">
+            {doMCQ(this.state.values.status) ? (
+              <div className="col-5 col-sm-5 col-lg-6">
+                <button className="btn btn-secondary btn-lg btn-block shadow" type="submit" >QCM</button>
+                <small className="text-secondary">Effectuer votre QCM</small>
+              </div>
+            ) : null}
+            {chooseInterview(this.state.values.status) ? (
+              <div className="col-5 col-sm-5 col-lg-6">
+                <button className="btn btn-secondary btn-lg btn-block shadow" type="submit" >Entretien</button>
+                <small className="text-secondary">Programmer votre entretien</small>
+              </div>
+            ) : null}
+          </div>
           <CreateApplicationForm editMode={false} values={this.state.values} />
         </div>
       </div>
